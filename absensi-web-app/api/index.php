b6286eda0c4214772519f832ccd64c491a9d23fb
awand795 <awand795@gmail.com>
2026-06-18 15:10:09 +0700

    debug: check vendor autoload

--- absensi-web-app/api/index.php
+++ absensi-web-app/api/index.php
@@ -1,3 +1,15 @@
 <?php
-echo "PHP is working. Current directory: " . __DIR__;
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+echo "Checking vendor folder... ";
+$autoload = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($autoload)) {
+    echo "Autoload found! Loading... ";
+    require $autoload;
+    echo "Autoload success!";
+} else {
+    echo "Autoload NOT FOUND at: " . $autoload;
+}
 exit;
